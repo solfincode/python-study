ff43193d3d92bcebebd716cbf4ed86cdec870643
Q <q@Q.local>
2019-12-25 21:05:51 +0900

    first commit

---
+++ 01_variable/app.py
@@ -0,0 +1,10 @@
+# variable to store data such as number, string, boolean
+price = 200
+weight = 100
+
+# variable for string
+word = "I am working on it"
+
+# variable for boolean variable
+checked = True
+
